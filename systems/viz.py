8a98f49b85c57553b8801364597f61fa33f7774e
Alex Bedig <hydrojumbo@users.noreply.github.com>
2023-08-27 16:32:44 -0400

    Use record nodes to display initial conditions and max (if not infinite).

--- systems/viz.py
+++ systems/viz.py
@@ -8,14 +8,16 @@ from graphviz import Digraph
 from . import parse
 from .errors import ParseException
 
-
 def as_dot(model, rankdir="TB", label=""):
     mapping = {s.name: str(i) for i, s in enumerate(model.stocks)}    
     dot = Digraph(comment=model.name)
     dot.attr(rankdir=rankdir)
     dot.attr(label=label)
     for stock in model.stocks:
-        dot.node(mapping[stock.name], stock.name)
+        if (str(stock.maximum) == 'inf'):
+            dot.node(mapping[stock.name], '{' + f'{stock.name}|Initial: {stock.initial}' + '}', shape='record')
+        else:    
+            dot.node(mapping[stock.name], '{' + f'{stock.name}|Initial: {stock.initial}|Max: {stock.maximum}' + '}', shape='record')
 
     for flow in model.flows:
         source_id = mapping[flow.source.name]
